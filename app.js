e19847c9c576b3c25047b959df70550b061ee1b0
aliabdulazeez <aliabdulazeez01@gmail.com>
2021-01-27 20:53:41 +0900

    3 katas added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Editor's note:
 // All the code below are code snippets from the course + my own spaghetti code^^
 
+/*
 
 //DESTRUCTURING:
 
@@ -22,3 +23,55 @@ function calculatePrice({
   
   const bill = calculatePrice({ tip: 0.15, total:150 });
   // 187.5
+
+
+
+
+
+  //Code Wars
+  problem 1
+
+  function sumTwoSmallestNumbers(numbers){  
+    numbers = numbers.sort(function(a, b){return a - b; });
+    return numbers;
+  };
+
+console.log(sumTwoSmallestNumbers([7, 46, 5, 3,12,1,23,45,56,567,7,8,9,0,11,22,33,44,55,66,77,88,99,00,00,12,12,32,2222,33]))
+
+
+
+//problem 2
+
+function getSum( a,b )
+{
+   //Good luck!
+  let arr = [a, b]
+  arr.sort(function(a, b){ return a - b;} );
+
+  let sum = 0;
+  for(let i = arr[0]; i <= arr[1]; i++){
+    sum += i;
+  }
+  return sum;
+  
+}
+
+console.log(getSum(1, 1));
+
+*/
+
+//Problem 3
+
+function accum(s) {
+  // your code
+ let newArr = s.split('');
+ 
+  let haha = newArr.map((letter, index) => {
+    console.log(letter, index)
+  })
+  
+  return haha.join(' ')
+
+}
+
+console.log(accum('Azeez'));
